e0ddc3c171ac9c032812b3e1f1b9450441220049
JessicaPamela <jpzcode@gmail.com>
2019-04-19 22:39:26 -0500

    getElementsByClassName

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@
   'use strict';
   document.addEventListener('DOMContentLoaded', function(){
     
-    //Selección de elementos por su ID
+    /* Selección de elementos por su ID */
     //var logo = document.getElementById('logo'); 
-    var navegacion = document.getElementById('navegacion');
-    navegacion.style.display = "none";
-      
+    // var navegacion = document.getElementById('navegacion');
+    // navegacion.style.display = "none";
     
+    /* Selección de elementos por CLASES */
+    var navegacion = document.getElementsByClassName('navegacion');
+    console.log(navegacion[0]);
     
   });
   
