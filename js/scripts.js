8d5a8ccc81dca7912f0c34920812fc5f96b9599f
JessicaPamela <jpzcode@gmail.com>
2019-04-19 22:31:35 -0500

    getElementByID

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@
   'use strict';
   document.addEventListener('DOMContentLoaded', function(){
     
-    
-      
+    //Selección de elementos por su ID
+    //var logo = document.getElementById('logo'); 
+    var navegacion = document.getElementById('navegacion');
+    navegacion.style.display = "none";
       
     
     
